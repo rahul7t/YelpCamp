4b4e247ac86cefaf09b5005b46add1c1dda10ec9
sharkbait07 <sharkbait07@gmail.com>
2018-01-19 22:15:17 -0500

    Adding bootstrat ui elements, adding camp site functionality

--- v1/app.js
+++ v1/app.js
@@ -1,21 +1,45 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+
+  var campgrounds = [
+    {name: "Toconos", image: "https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg" },
+    {name: "Black Forest", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
+    {name: "Hill Range", image: "https://farm8.staticflickr.com/7113/7482049174_560bf194ec.jpg"},
+    {name: "Toconos", image: "https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg" },
+    {name: "Black Forest", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
+    {name: "Hill Range", image: "https://farm8.staticflickr.com/7113/7482049174_560bf194ec.jpg"},
+    {name: "Toconos", image: "https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg" },
+    {name: "Black Forest", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
+    {name: "Hill Range", image: "https://farm8.staticflickr.com/7113/7482049174_560bf194ec.jpg"},
+    {name: "Toconos", image: "https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg" },
+    {name: "Black Forest", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
+    {name: "Hill Range", image: "https://farm8.staticflickr.com/7113/7482049174_560bf194ec.jpg"}
+  ];
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req,res){
-  var campgrounds = [
-    {name: "Toconos", image: "https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg" },
-    {name: "Black Forest", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
-    {name: "Hill Range", image: "https://farm8.staticflickr.com/7113/7482049174_560bf194ec.jpg"}
-  ]
+
   res.render("campgrounds",{ campgrounds: campgrounds });
 });
 
+app.post("/campgrounds", function(req,res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampgrounds = {name:name, image:image};
+  campgrounds.push(newCampgrounds);
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new",function(req,res){
+  res.render("new.ejs");
+});
 
 
 app.listen(3000,function(){
